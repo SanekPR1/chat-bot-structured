d54dda3e167f6c547a481a28db67802dd0e5b99c
Aleksandr Makridin <epam_amakridin@exactsciences.com>
2025-04-06 15:00:31 -0500

    #0000: initial commit

---
+++ src/main/java/com/example/makridin/chat_bot_structured/ChatBotStructuredApplication.java
@@ -0,0 +1,13 @@
+package com.example.makridin.chat_bot_structured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotStructuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotStructuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makridin/chat_bot_structured/ChatBotStructuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makridin.chat_bot_structured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotStructuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
